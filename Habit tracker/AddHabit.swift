6bf744e016d0056f9eb882c9d3e4c72abfbc64c6
Viachaslau Hryniuk <qwerty33566666@gmail.com>
2023-10-15 23:33:06 +0200

    FINAL

--- Habit tracker/AddHabit.swift
+++ Habit tracker/AddHabit.swift
@@ -2,23 +2,26 @@
 //  AddHabit.swift
 //  Habit tracker
 //
-//  Created by Слава Гринюк on 11.10.23.
+//  Created by Viachaslau Hryniuk on 11.10.23.
 //
 
 import SwiftUI
 import ContributionChart
 struct AddHabit: View {
-    @Environment(\.dismiss) private var dismiss
-    @State private var emptyMsgFlag = false
+    @ObservedObject var habits: Habits
     @State private var name = ""
     @State private var description = ""
-    @ObservedObject var habits: Habits
     let time = ["5","10","15","30","60","90","Other"]
-    @State private var chosenGoal = ""
-    @State private var addInfo = ""
-    @State private var other = false
+    @State private var chosenGoal = "" // Chosen goal from array above
+    @State private var other = false // Other element flag
+    @State private var addInfo = "" // Custom goal
+    @FocusState var isInputActive: Bool // Description textfield focus
+    @FocusState private var campaignTitleIsFocussed: Bool // Date textfield focus
+    @State private var emptyMsgFlag = false // Empty alert flag
+    @Environment(\.dismiss) private var dismiss
+   
     var body: some View {
-        NavigationStack{
+      
             ZStack{
                 Rectangle()
                     .fill(Color("Text"))
@@ -26,63 +29,117 @@ struct AddHabit: View {
                 Rectangle()
                     .fill(.ultraThinMaterial)
                     .ignoresSafeArea()
-                VStack(spacing: 25){
-                    HStack{
+                
+                VStack(spacing: 25) { // main VStack with all stuff
+                    HStack{ // Title text
                         Text("Add a new habit")
                             .font(.largeTitle)
                             .fontWeight(.semibold)
                             .padding(20)
-                            Spacer()
+                        Spacer()
                         
                     }
                     .padding()
+                    // ------------
+                    
                     Divider()
-                    TextField("Name of the habit you want to nail", text: $name )
+                    
+                    
+                    TextField("Name of the habit you want to nail", text: $name ) //Textfield for name
                         .font(.custom("Big", size: 18))
                         .frame(width: 350,height: 50)
+                        .cornerRadius(20)
                         .foregroundColor(.black)
                         .background(Color("Cream"))
                         .multilineTextAlignment(.center)
-                        .cornerRadius(20)
+                        .submitLabel(.done)
                         .overlay(
                             RoundedRectangle(cornerRadius: 20)
                                 .stroke(Color("Sec"), lineWidth: 2)
                         )
-                    TextField("    Description and motivation for yourself", text: $description, axis: .vertical )
+                    
+                    
+                    
+                    
+                    TextField("    Description and motivation for yourself", text: $description, axis: .vertical ) //TextField for description
                         .font(.custom("Big", size: 18))
                         .frame(width: 350,height: 150)
+                        .cornerRadius(20)
                         .foregroundColor(.black)
                         .background(Color("Cream"))
                         .multilineTextAlignment(.center)
-                        .cornerRadius(20)
                         .overlay(
                             RoundedRectangle(cornerRadius: 20)
                                 .stroke(Color("Sec"), lineWidth: 2)
                         )
-                    VStack{
-                        Text("Days goal")
-                            .foregroundColor(.secondary.opacity(0.5))
-                            .font(.body)
-                            .fontWeight(.regular)
-                            .padding(.top,25)
-                        
-                        Picker("Days goal", selection: $chosenGoal) {
-                            ForEach(time, id: \.self){
-                                Text($0)
+                        .focused($campaignTitleIsFocussed)
+                        .onChange(of: description) { newValue in
+                            guard let newValueLastChar = newValue.last else { return }
+                            if newValueLastChar == "\n" {
+                                description.removeLast()
+                                campaignTitleIsFocussed = false
                             }
-                            
                         }
-                        .onChange(of: chosenGoal, perform: { _ in
-                            onReceive(days: chosenGoal)
-                        })
-                        .pickerStyle(.segmented)
-                        .padding()
-                        TextField("Enter amount of days", text: $addInfo )
-                            .keyboardType(.decimalPad)
-                            .multilineTextAlignment(.center)
-                            .isHidden(!other)
-                    }
-                   
+                        .submitLabel(.done)
+                       
+                        //----------
+                       
+                    NavigationStack{
+                        ZStack{
+                            Rectangle()
+                                .fill(Color("Cream"))
+                                .ignoresSafeArea()
+                        
+                            
+                            
+                            VStack{
+                                Text("Days goal")
+                                    .foregroundColor(.secondary.opacity(0.5))
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                    .padding(.top,25)
+                                
+                                
+                                
+                                
+                                Picker("Days goal", selection: $chosenGoal) {
+                                    ForEach(time, id: \.self){
+                                        Text($0)
+                                    }
+                                    
+                                }
+                                .padding()
+                                .onChange(of: chosenGoal, perform: { _ in
+                                    onReceive(days: chosenGoal)
+                                })
+                                .pickerStyle(.segmented)
+                                
+                                
+                                
+                                
+                                TextField("Enter amount of days", text: $addInfo )
+                                    .keyboardType(.decimalPad)
+                                    .multilineTextAlignment(.center)
+                                    .submitLabel(.done)
+                                    .isHidden(!other)
+                                    .focused($isInputActive)
+                                    .toolbar {
+                                        ToolbarItemGroup(placement: .keyboard) {
+                                            Spacer()
+                                            
+                                            Button("Done") {
+                                                isInputActive = false
+                                            }
+                                        }
+                                    }
+                                
+                                
+                                
+                                
+                            } //VStack ends
+                        } //ZStack ends
+                    
+                    }//NavStack Ends
                     .frame(width: 350,height: 175)
                     .background(Color("Cream"))
                     .cornerRadius(20)
@@ -91,10 +148,13 @@ struct AddHabit: View {
                             .stroke(Color("Sec"), lineWidth: 2)
                     )
                     
-                   
-        
+                    
                     Spacer()
-                    Button(action: {
+                    
+                    
+                    
+                    
+                    Button(action: { //Save button
                         if saveHabit() != 1{
                             dismiss()
                         }
@@ -102,9 +162,9 @@ struct AddHabit: View {
                     }, label: {
                         Image(systemName: "checkmark")
                         Text("Save")
-                           
-                    
-                            
+                        
+                        
+                        
                     })
                     
                     .foregroundColor(Color("Text"))
@@ -112,18 +172,18 @@ struct AddHabit: View {
                     .background(Color("Sec"))
                     .clipShape(Capsule())
                     
-                }
+                }// Main VStack ends
                 
             }
             
             
             
-        }
+        
         .alert("Fill all of the fields", isPresented: $emptyMsgFlag) {
             Button("OK", role: .cancel){}
         }
         
-    }
+    } // Body ends here
     func onReceive(days: String){
         if days == "Other"{
             addInfo = ""
@@ -135,6 +195,7 @@ struct AddHabit: View {
             
         }
     }
+    
     func saveHabit() -> Int{
         
         if !addInfo.isEmpty{
@@ -149,6 +210,7 @@ struct AddHabit: View {
         return 0
     }
 }
+
 extension View {
     @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
         if hidden {
@@ -160,6 +222,7 @@ extension View {
         }
     }
 }
+
 struct AddHabit_Previews: PreviewProvider {
     static var previews: some View {
         AddHabit(habits: Habits())
